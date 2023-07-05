253ad0b0975f8fa0b1c3ef5ccbd050e76839eff3
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-05 10:34:25 -0500

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [creatureList, setCreatureList] = useState([
-    { name: 'Unicorn', origin: 'Britain' },
-    { name: 'Sphinx', origin: 'Egypt' },
-    { name: 'Jackalope', origin: 'America' }
-  ])
+    { name: "Unicorn", origin: "Britain" },
+    { name: "Sphinx", origin: "Egypt" },
+    { name: "Jackalope", origin: "America" },
+  ]);
 
   return (
     <>
@@ -20,6 +20,38 @@ function App() {
             {/* We can use JSON.stringify() to dump raw data into our DOM */}
             {JSON.stringify(creatureList)}
           </pre>
+          <p>
+            Getting one creature looks like:
+            {creatureList[0].name} is from {creatureList[0].origin}
+          </p>
+          {/* map method: looping over creatureList, for each creature: return new <ul> */}
+          {/* .map always loop through array and return new array */}
+         <ul>
+          {creatureList.map( function (creature) {
+             console.log(creature);
+             // key let React uniquely identify each element 
+             // we want to use something unique for a key - usually database id
+            return (
+              <li
+                key={creature.name}>{creature.name} is from {creature.origin}
+              </li>
+            );
+          })}
+          </ul>
+          {/* VARIATION of .map ARROW FUNCTION*/}
+          <ul>
+          {creatureList.map((creature) => {
+            return (
+              <li>
+                {creature.name} is from {creature.origin}
+              </li>
+            );
+          })}
+          </ul>
+          {/* VARIATION of .map IMPLICIT FUNCTION: no parenthesis around parameters */}
+          <ul>
+          {creatureList.map(creature => <li>{creature.name} is from {creature.origin}</li>)}
+          </ul>
         </div>
       </div>
     </>
